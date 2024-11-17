d24632527243d3f8f8f9289f9530b22996759f0a
maria augusta bistafa <mariaaugustabistafa@gmail.com>
2024-11-17 14:06:52 -0300

    ordenei a lista de prioridades e limpei o input radio no js

--- script.js
+++ script.js
@@ -14,19 +14,14 @@ function concluirTarefa(checkbox) {
 
     document.getElementById('todo-list-checked').appendChild(marcarCheckbox);
 
-    const deletarIcone = document.getElementsByClassName('custom-icon');
-        for(var i = 0; i < deletarIcone.length; i++){
-            deletarIcone[i].remove()
-        }
+    const deletarIcone = marcarCheckbox.querySelector('.custom-icon');
+    
+    deletarIcone.remove();
+
     checkbox.remove();
+
     marcarCheckbox.style.backgroundColor = 'rgb(253, 243, 244)'
     marcarCheckbox.style.textDecoration = "line-through";
-    
-    const listaDeTarefas = document.getElementsByClassName('list-prioridade')
-
-    if(listaDeTarefas){
-        listaDeTarefas.innerHTML =''
-    }
 }
  
     
@@ -59,28 +54,38 @@ function adicionarTarefa(event){
             
 
             const prioridade = document.getElementsByName('prioridade');
-
+            
 
             //COLOCAR COR NAS PRIORIDADES DIFERENTES E ORDENAR A LISTA/ 
             for(var i = 0; i<prioridade.length; i++){
                 if(prioridade[0].checked){ 
                     criarTarefa.style.backgroundColor = 'rgb(248, 4, 28)';
-                    document.getElementById('item-p1').appendChild(criarTarefa);
+                    document.getElementById('todo-list-p1').appendChild(criarTarefa);
+                    break;
                 }
                 if(prioridade[1].checked){
                     criarTarefa.style.backgroundColor = 'rgb(243, 90, 90)';
-                    document.getElementById('item-p2').appendChild(criarTarefa);
+                    document.getElementById('todo-list-p2').appendChild(criarTarefa);
+                    break;
                 }
                 if(prioridade[2].checked){
                     criarTarefa.style.backgroundColor = 'rgb(243, 191, 197)';
-                    document.getElementById('item-p3').appendChild(criarTarefa);
-                }             
+                    document.getElementById('todo-list-p3').appendChild(criarTarefa);
+                    break;
+                } 
+                if(prioridade[i]){
+                    document.getElementById('todo-list-unchecked').appendChild(criarTarefa);
+                    break; // o break que foi definitivo para deixar a lista em ordem
+                }
             }
-
             
-            document.getElementById('todo-list-unchecked').appendChild(criarTarefa);
-        
-            document.getElementById('task').value = ""; //FAZ COM QUE O INPUT FIQUE EM BRANCO DEPOIS DE DAR ENTER
+            //DEIXAR O INPUT EM BRANCO DEPOIS DE DAR ENTER
+            document.getElementById('task').value = ""; 
+
+            //DESMARCAR OS INPUTS TIPO RADIO
+            for (var i = 0; i < prioridade.length; i++) {
+                prioridade[i].checked = false; 
+            }
 
         } else {
             alert("Por favor, insira uma tarefa.");
